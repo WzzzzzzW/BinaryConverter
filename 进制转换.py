c9ea4ae956b31fde24ead1f53ace6304d7be63bf
WiliW <scp111112@outlook.com>
2024-09-27 19:16:48 +0800

    *解决了输入只能是整型的问题

--- 进制转换.py
+++ 进制转换.py
@@ -1,14 +1,18 @@
 while True:
-    a = int(input("输入待转值:"))
+    a = str(input("输入待转值:"))
     d = int(input("代转值是几进制:"))
     b = int(input("转换成几进制:"))
     s = [] #一个空列表，用来存储模
     c=""   #输出值
     e = 0  #a转化为10进制的数
-    f = 1  #计算权值  
+    f = 1  #计算权值
     for i in str(a):
-        e +=int(i)*d**(len(str(a))-f)
+        if ord(i)>=65:
+            e+=int(ord(i)-55)*d**(len(str(a))-f)
+        else:
+            e+=int(i)*d**(len(str(a))-f)
         f+=1
+        
     if b<=36:
         while e>=b:
             s.append(e%b)
